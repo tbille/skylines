b2ebcea2677bb5c81f71c2dcb78f65eebea70de1
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-27 20:22:27 +0200

    tests/events: Remove unintended octal literal prefixes

--- tests/data/events.py
+++ tests/data/events.py
@@ -6,7 +6,7 @@ from skylines.model import Event
 def flight_comment(actor, flight, flight_comment, **kwargs):
     return Event(
         type=Event.Type.FLIGHT_COMMENT,
-        time=datetime(2017, 02, 11, 12, 34, 56),
+        time=datetime(2017, 2, 11, 12, 34, 56),
         actor=actor,
         flight=flight,
         flight_comment=flight_comment,
@@ -16,7 +16,7 @@ def flight_comment(actor, flight, flight_comment, **kwargs):
 def flight(actor, flight, **kwargs):
     return Event(
         type=Event.Type.FLIGHT,
-        time=datetime(2017, 02, 12, 12, 34, 56),
+        time=datetime(2017, 2, 12, 12, 34, 56),
         actor=actor,
         flight=flight,
     ).apply_kwargs(kwargs)
@@ -25,7 +25,7 @@ def flight(actor, flight, **kwargs):
 def follower(actor, user, **kwargs):
     return Event(
         type=Event.Type.FOLLOWER,
-        time=datetime(2017, 02, 13, 12, 34, 56),
+        time=datetime(2017, 2, 13, 12, 34, 56),
         actor=actor,
         user=user,
     ).apply_kwargs(kwargs)
@@ -34,7 +34,7 @@ def follower(actor, user, **kwargs):
 def new_user(actor, **kwargs):
     return Event(
         type=Event.Type.NEW_USER,
-        time=datetime(2017, 02, 14, 12, 34, 56),
+        time=datetime(2017, 2, 14, 12, 34, 56),
         actor=actor,
     ).apply_kwargs(kwargs)
 
@@ -42,7 +42,7 @@ def new_user(actor, **kwargs):
 def club_join(actor, club, **kwargs):
     return Event(
         type=Event.Type.CLUB_JOIN,
-        time=datetime(2017, 02, 15, 12, 34, 56),
+        time=datetime(2017, 2, 15, 12, 34, 56),
         actor=actor,
         club=club,
     ).apply_kwargs(kwargs)
